a243ae702170a594572c83e9e7949101b5c6b1b6
lzkunnnnn <telangou@asd.com>
2025-04-10 11:29:53 +0800

    lzk

--- src/layouts/components/sideBar/logo.vue
+++ src/layouts/components/sideBar/logo.vue
@@ -1,7 +1,7 @@
 <template>
   <router-link to="/index" class="logo-wrapper ">
     <svg-icon icon="guide" class="verticalMiddle" :size="30" />
-    <div class="logo-title verticalMiddle">智慧交通</div>
+    <div class="logo-title verticalMiddle">智慧交通管理平台</div>
   </router-link>
 </template>
 
